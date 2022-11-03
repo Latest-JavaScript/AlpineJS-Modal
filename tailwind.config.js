000a738f39e0b0b12ee619233b82cd3619c36d1f
Sanan Ali <Alisanan620@gmail.com>
2022-11-03 20:06:15 +0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mont: "Montserrat",
+        heading: 'Nunito Sans',
+        primary: "Asap",
+
+      },
+      colors: {
+        background: "#F3F5F7",
+        secondary: "#373737",
+        primary: "#0576F0",
+        header: "#2D2C2C",
+        button: "#464646",
+      },
+      fontSize: {
+        heading: "22px",
+        subheading: "18px",
+      },
+    },
+  },
+  plugins: [],
+};
